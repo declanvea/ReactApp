26e96de117ac0b997f67dfea01fa841db98aafc5
Declan Vea <declanpvea@gmail.com>
2017-09-13 16:22:42 -0400

    added quake.id and fixed class issue

--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -6,20 +6,22 @@ export default class EarthquakeList extends Component {
   render() {
     return(
       <div className="quake-list">
-        {earthquakes.features.map(quakes =>
-          <div className="col-sm-6" key={quakes}>
-            <div className="card" >
-              <div className="card-block">
-                <h4 className="card-title">{quakes.properties.place}</h4>
-                <h6 className="card-subtitle mb-2 text-muted">Magnitude: {quakes.properties.mag}</h6>
-                <h6 className="card-subtitle mb-2 text-muted">Time: {moment(quakes.properties.time).format('llll')}</h6>
-                <p className="card-text">Coordinates: {quakes.geometry.coordinates}</p>
-                <a href={earthquakes.metadata.url} className="card-link">USGS Event Link</a>
+        <div className="row">
+          {earthquakes.features.map(quakes =>
+            <div className="col-sm-6" key={quakes.id}>
+              <div className="card" >
+                <div className="card-block">
+                  <h4 className="card-title">{quakes.properties.place}</h4>
+                  <h6 className="card-subtitle mb-2 text-muted">Magnitude: {quakes.properties.mag}</h6>
+                  <h6 className="card-subtitle mb-2 text-muted">Time: {moment(quakes.properties.time).format('llll')}</h6>
+                  <p className="card-text">Coordinates: {quakes.geometry.coordinates}</p>
+                  <a href={quakes.properties.url} className="card-link">USGS Event Link</a>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
         </div>
-      )
-    }
+      </div>
+    )
   }
+}
